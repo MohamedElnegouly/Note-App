ae7eba5faecd679ed29195ce7c1c50fc91070df8
mohamed yasser <medoyasser511@gmail.com>
2024-09-17 21:05:50 +0300

    List view builder

--- lib/widgets/Notes_View_Body.dart
+++ lib/widgets/Notes_View_Body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Custom_App_Bar.dart';
-import 'Custom_item.dart';
+import 'Custom_NotesListView.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({
@@ -17,7 +17,7 @@ class NotesViewBody extends StatelessWidget {
             height: 30,
           ),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
