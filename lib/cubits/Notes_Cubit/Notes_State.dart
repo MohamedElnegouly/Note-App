be0a0a58f171b8a7d68191bde82a1f51cbc92112
mohamed yasser <medoyasser511@gmail.com>
2024-09-23 02:51:47 +0300

    display notes list without data

--- lib/cubits/Notes_Cubit/Notes_State.dart
+++ lib/cubits/Notes_Cubit/Notes_State.dart
@@ -1,17 +1,3 @@
-import 'package:note_app/Models/note_model.dart';
-
 abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
-
-class NotesLoading extends NotesState {}
-
-class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-  NotesSuccess(this.notes);
-}
-
-class NotesFaliure extends NotesState {
-  final String errMessage;
-  NotesFaliure(this.errMessage);
-}
--- lib/cubits/Notes_Cubit/Notes_cubit.dart
+++ lib/cubits/Notes_Cubit/Notes_cubit.dart
@@ -7,13 +7,9 @@ import '../../Constants.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
   FetchAllNotes() {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFaliure(e.toString()));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
