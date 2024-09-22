2a147b77f97678b622277c6f50a0e9955ccc1f02
mohamed yasser <medoyasser511@gmail.com>
2024-09-22 18:28:18 +0300

    solve regesterAdapter exception

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ import 'package:note_app/simple_bloc_observer.dart';
 void main() async {
   //initialize hive
   await Hive.initFlutter();
-  //create hive box (collection) , <model> and its name
-  await Hive.openBox<NoteModel>(kNotesBox);
   // to tell hive to storage NoteModel
   Hive.registerAdapter(NoteModelAdapter());
+  //create hive box (collection) , <model> and its name
+  await Hive.openBox<NoteModel>(kNotesBox);
   // to trigger observer
   Bloc.observer = SimpleBlocObserver();
   runApp(const NotesApp());
